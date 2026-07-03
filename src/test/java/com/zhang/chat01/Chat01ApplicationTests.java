91010e2ca598a644805a16bd37f7ca17c305a2c4
张帅 <zhangshuai3@bonc.com.cn>
2026-07-03 10:38:15 +0800

    file

---
+++ src/test/java/com/zhang/chat01/Chat01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.chat01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
